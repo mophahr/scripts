cb13750f0305b1ad3aefe660f9faa8853b0eced3
moritz <msch@pks.mpg.de>
2013-02-06 11:06:24 +0100

    initialisation

---
+++ mail_sync.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+scp vesta:~/.mutt_aliases ~/
+scp vesta:~/.muttrc ~/
+scp vesta:~/.signature ~/
---
+++ vga1_left.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output VGA1 --preferred --output LVDS1 --mode 1366x768 --right-of VGA1
